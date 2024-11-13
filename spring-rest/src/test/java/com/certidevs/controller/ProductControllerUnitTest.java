07f234f5a4a6ee24f881e4e09d67a7c1e8301a69
alansastre <alan@alansastre.co>
2024-11-13 11:01:25 +0100

    testing unitario productController findAll findById

--- spring-rest/src/test/java/com/certidevs/controller/ProductControllerUnitTest.java
+++ spring-rest/src/test/java/com/certidevs/controller/ProductControllerUnitTest.java
@@ -2,16 +2,18 @@ package com.certidevs.controller;
 
 import com.certidevs.model.Product;
 import com.certidevs.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -28,7 +30,6 @@ class ProductControllerUnitTest {
     // SUT - System Under Test
     @InjectMocks
     private ProductController productController;
-
 
     @Test
     @DisplayName("Buscar todos los productos")
@@ -75,13 +76,76 @@ class ProductControllerUnitTest {
     }
 
     @Test
+    @DisplayName("Buscar producto por id - Sí encontrado")
     void findById_Found() {
+        var product = Product.builder().id(1L).name("prod1").price(10d).build();
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
 
+        var response = productController.findById(1L);
+
+        // asserts de JUnit
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(product, response.getBody());
+        assertEquals(product.getId(), response.getBody().getId());
+
+        // verificaciones de mockito, interacciones con los mocks:
+        verify(productRepository).findById(1L);
+    }
+
+    @Test
+    @DisplayName("Buscar producto por id - NO encontrado")
+    void findById_NotFound() {
+        when(productRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        var exception = assertThrows(
+                ResponseStatusException.class,
+                () -> productController.findById(3L)
+        );
+
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+    }
+
+
+    @Test
+    @DisplayName("Buscar producto con filtro Query By Example")
+    void findAllFilteringByExample() {
+
+        var product = Product.builder().price(10d).build();
+        List<Product> products = List.of(
+                Product.builder().id(1L).name("prod1").price(10d).build(),
+                Product.builder().id(2L).name("prod2").price(100d).build()
+        );
+        // when(productRepository.findAll((Example<Product>) any())).thenReturn(products);
+        when(productRepository.findAll(ArgumentMatchers.<Example<Product>>any())).thenReturn(products);
+
+        var response = productController.findAllFilteringByExample(product);
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(products, response.getBody());
+        verify(productRepository).findAll(ArgumentMatchers.<Example<Product>>any());
 
     }
 
     @Test
-    void findById_NotFound() {
+    @DisplayName("Crear producto nuevo - No debe tener id - Bad Request")
+    void create_badRequest() {
+        Product product = Product.builder().id(1L).build();
+        var exception = assertThrows(ResponseStatusException.class, () -> productController.create(product));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Crear producto nuevo - OK")
+    void create_ok() {
+
+    }
+
+    @Test
+    @DisplayName("Crear producto nuevo - Conflicto por excepción de repositorio")
+    void create_conflict () {
 
     }
 
