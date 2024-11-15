a7bdd43e7df55aeab3ee0746315e619265a94d89
alansastre <alan@alansastre.co>
2024-11-15 12:49:52 +0100

    testing integración completa create con objectMapper
    objectMapper.writeValueAsString y objectMapper.readValue

--- spring-rest/src/test/java/com/certidevs/controller/ProductControllerIntegrationTest.java
+++ spring-rest/src/test/java/com/certidevs/controller/ProductControllerIntegrationTest.java
@@ -2,19 +2,24 @@ package com.certidevs.controller;
 
 import com.certidevs.model.Product;
 import com.certidevs.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,6 +38,8 @@ public class ProductControllerIntegrationTest {
     // NO ES UN MOCK: inyectar repositorio real ProductRepository
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
 
 //    @BeforeEach
 //    void setUp() {
@@ -47,9 +54,9 @@ public class ProductControllerIntegrationTest {
     @Test
     void findAll() throws Exception {
         productRepository.saveAll(List.of(
-           Product.builder().name("prod1").price(30d).active(true).build(),
-           Product.builder().name("prod2").price(30d).active(true).build(),
-           Product.builder().name("prod3").price(30d).active(true).build()
+                Product.builder().name("prod1").price(30d).active(true).build(),
+                Product.builder().name("prod2").price(30d).active(true).build(),
+                Product.builder().name("prod3").price(30d).active(true).build()
         ));
         mockMvc.perform(get("/api/products"))
                 .andExpect(status().isOk())
@@ -62,7 +69,7 @@ public class ProductControllerIntegrationTest {
     }
 
     @Test
-    void findAllWithCalculatedIVA()throws Exception {
+    void findAllWithCalculatedIVA() throws Exception {
         productRepository.saveAll(List.of(
                 Product.builder().name("prod1").price(10d).active(true).build(),
                 Product.builder().name("prod2").price(100d).active(true).build()
@@ -75,5 +82,61 @@ public class ProductControllerIntegrationTest {
 
         productRepository.findAll().forEach(System.out::println);
     }
+
+    @Test
+    void findAll_QBE() throws Exception {
+        productRepository.saveAll(List.of(
+                Product.builder().name("prod1").price(10d).active(true).build(),
+                Product.builder().name("prod2").price(20d).active(true).build(),
+                Product.builder().name("prod2").price(20d).active(true).build(),
+                Product.builder().name("prod2").price(30d).active(true).build()
+        ));
+
+        var productFilter = Product.builder().price(20d).build();
+
+        mockMvc.perform(post("/api/products/search")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productFilter))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].price").value(20d))
+                .andExpect(jsonPath("$[1].price").value(20d));
+    }
+
+    @Test
+    void create_OK()  throws Exception{
+        var product = Product.builder().name("prod1").price(20d).build();
+
+        MvcResult result = mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(product))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.name").value("prod1"))
+                .andExpect(jsonPath("$.price").value(20d))
+                .andReturn();
+
+        String location = result.getResponse().getHeader("location");
+        assertNotNull(location); // "/api/products/1"
+
+        // Extraer el id del location
+        Long locationId = Long.valueOf(location.substring(location.lastIndexOf("/") + 1));
+        //Long locationId = Long.valueOf(location.split("/")[location.split("/").length - 1]);
+
+        // Extraer el id del la respuesta JSON
+        Product productResponse = objectMapper.readValue(result.getResponse().getContentAsString(), Product.class);
+        assertEquals(locationId, productResponse.getId());
+
+        mockMvc.perform(get(location))
+                .andExpect(jsonPath("$.id").value(productResponse.getId()));
+
+        assertTrue(productRepository.existsById(locationId));
+        // productRepository.findAll().get(0)
+        // productRepository.findById(id).get()
+    }
+
+
 
 }
